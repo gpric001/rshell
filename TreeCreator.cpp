5acdb7c164dc2bc02bdc674094e1da6b4a7c23df
Guthrie Price <gprice1129@gmail.com>
2016-02-10 16:55:22 -0800

    Fixed numerous bugs

--- TreeCreator.cpp
+++ TreeCreator.cpp
@@ -2,11 +2,11 @@
 
 TreeCreator::TreeCreator() {};
 
-CommandComponent* TreeCreator::create(tokens& input){
+CommandComponent* TreeCreator::create(const tokens& input){
     build(0, input);
 }
 
-CommandComponent* TreeCreator::build(int start, tokens& input){
+CommandComponent* TreeCreator::build(int start, const tokens& input){
     for(int i = start; i < input.size(); i++){
         if(input[i][0] == ";"){
             CommandComponent* left = buildSub(start, i, input);
@@ -17,13 +17,13 @@ CommandComponent* TreeCreator::build(int start, tokens& input){
     return buildSub(start, input.size(), input);
 }
 
-CommandComponent* TreeCreator::buildSub(int start, int end, tokens& input){
+CommandComponent* TreeCreator::buildSub(int start, int end, const tokens& input){
     if((end - start - 1) == 0)
         return buildCmd(input[start]);
-    std::string connectorToken = tokens[start+1][0];
+    std::string connectorToken = input[start+1][0];
     if(connectorToken == "||" || connectorToken == "&&"){
-        CommandComponent* left = buildCmd(tokens[start]);
-        CommandComponent* right = buildSub(start+2, end, tokens);
+        CommandComponent* left = buildCmd(input[start]);
+        CommandComponent* right = buildSub(start+2, end, input);
         return buildConnector(connectorToken, left, right);
     }
     else{
@@ -31,7 +31,7 @@ CommandComponent* TreeCreator::buildSub(int start, int end, tokens& input){
     }
 }
 
-CommandComponent* TreeCreator::buildConnector(std::string& token, 
+CommandComponent* TreeCreator::buildConnector(const std::string& token, 
                                 CommandComponent* left, 
                                 CommandComponent* right){
     CommandComponent* connector;
@@ -47,18 +47,18 @@ CommandComponent* TreeCreator::buildConnector(std::string& token,
     return connector;
 }
 
-CommandComponent* TreeCreator::buildCmd(tokens& input){
-    std::string cmdName = input[0];
+CommandComponent* TreeCreator::buildCmd(const std::vector<std::string>& cmd){
+    std::string cmdName = cmd[0];
     CommandComponent* command;
     if(cmdName == "echo")
-        command = new Echo();
+        command = new Echo;
     else if(cmdName == "cd")
-        command = new Cd();
+        command = new Cd;
     else if(cmdName == "exit")
-        command = new Exit();
+        command = new Exit;
     else
-        command = new BinCmd();
-    command->set(cmdName, input);
+        command = new BinCmd;
+    command->set(cmdName, cmd);
     return command;
 }
 
